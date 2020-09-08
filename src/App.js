fb1b79f17e9c9f96583b361041362a2073846802
anjurul <anjurul.ewallet@gmail.com>
2020-09-08 21:01:01 +0400

    added user-pofile-app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
+import UserProfile from './components/UserProfile/UserProfile';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <UserProfile></UserProfile>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,6 +6,11 @@ const Header = () => {
     return (
         <div className="header">
            <img src={logo} alt=""/>
+           <nav>
+               <a href="/user">User Profile</a>
+               <a href="/friends">Friends</a>
+               <a href="/manage">Manage Inventory</a>
+           </nav>
         </div>
     );
 };
